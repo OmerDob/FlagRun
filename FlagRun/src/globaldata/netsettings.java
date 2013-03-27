bcada1ab9eee24d6aae47247f2d5ef87b417149f
aravmaxim <aravmaxim@gmail.com>
2013-03-27 16:41:19 +0200

    bla bla

---
+++ FlagRun/src/globaldata/netsettings.java
@@ -0,0 +1,8 @@
+package globaldata;
+
+// TODO : find better way to store settings
+public class netsettings {
+	public static int CLIENT_PORT_UDP = 6667;
+	public static int CLIENT_PORT_TCP = 6668;
+	public static String SERVER_IP = "79.181.216.29";
+}
